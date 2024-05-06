148ae30a8bcd525e96921665181cf16727b391a6
horvathg13 <horvathgabor338@gmail.com>
2024-05-07 00:12:49 +0200

    Cookie handler function and dependecies added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
 import Header from "./Header/header";
-import React from "react";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 import Home from "./components/home";
 import AboutMe from "./components/aboutme";
 import Footer from "./Footer/footer";
@@ -14,8 +14,53 @@ import Research from "./components/research";
 import ContactMe from "./components/contactMe";
 import ErrorPage from "./error-page";
 import Loader from "./loader";
+import Cookies from "universal-cookie";
+import FloatingBanner from "./components/FloatingBanner";
 
 function App() {
+    const [decisionMade, setDecisionMade] = useState(true) // start with true to avoid flashing
+    const cookies = useMemo(() => new Cookies(), []);
+
+    function gtag() {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push(arguments);
+    }
+
+    const sendConsent = useCallback((consent) => {
+        localStorage.setItem("consent", JSON.stringify(consent))
+        gtag('consent', 'default', consent);
+        gtag('js', new Date())
+        gtag('config', 'G-X8WM43FSKH')
+
+    }, []);
+
+    useEffect(() => {
+        if (cookies.get("COOKIE_NAME") !== undefined) {
+            setDecisionMade(true)
+            if(localStorage.getItem("consent")){
+                sendConsent(JSON.parse(localStorage.getItem("consent")))
+            }
+        } else {
+            setDecisionMade(false)
+        }
+    }, [cookies, setDecisionMade, sendConsent])
+
+    const handleDecision = (outcome) => {
+        const consent = {
+            'ad_storage': outcome,
+            'analytics_storage': outcome,
+            'ad_user_data': outcome,
+            'ad_personalization': outcome,
+        }
+
+        cookies.set("COOKIE_NAME", consent, {
+            expires: new Date(new Date().setMinutes(new Date().getMinutes() + 60)),
+            path: "/"
+        })
+
+        sendConsent(consent)
+        setDecisionMade(true)
+    }
   const Root = ()=>{
     return(
         <>
@@ -23,6 +68,22 @@ function App() {
             <div id="content">
                 <Outlet/>
             </div>
+            { decisionMade ? (
+                <></>
+            ) : (
+                <FloatingBanner
+                    onAccept={
+                        () => {
+                            handleDecision("granted")
+                        }
+                    }
+                    onDeny={
+                        () => {
+                            handleDecision("denied")
+                        }
+                    } />
+            )
+            }
             <div>
                 <Footer/>
             </div>
